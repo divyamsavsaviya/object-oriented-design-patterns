cf023ef9ed872465b60a2d8e25e0dd23fa097f6e
Divyam Savsaviya <59193532+divyamsavsaviya@users.noreply.github.com>
2024-03-25 18:21:20 -0700

    Add Decorator pattern

---
+++ src/Decorator/PlainPizza.java
@@ -0,0 +1,13 @@
+package Decorator;
+
+public class PlainPizza implements Pizza {
+    @Override
+    public String getDescription() {
+        return "Thin dough";
+    }
+
+    @Override
+    public double getCost() {
+        return 4.00;
+    }
+}
